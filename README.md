1fab515aa75847d3074d5c7471b1f19726c039f7
lee <leerwilson@gmail.com>
2016-10-10 21:00:46 -0400

    adding pages

---
+++ README.md
@@ -0,0 +1,24 @@
+# project-1
+
+That Darn Cat!
+
+I this game your cat is on the shelf and is knocking items off of it. 
+You can click to move items back onto the shelf. 
+If the cat pushes items off the edge of the shelf they fall and break. 
+If several (10?) items fall off, you lose the game. 
+
+The game can go on indefinitely as long as the player keeps items on the shelf. 
+
+Technologies: HTML, CSS with flexbox, jQuery and vanilla javascript.
+credits: 
+cat from http://opengameart.org/content/cat-dog-free-sprites license https://creativecommons.org/publicdomain/zero/1.0/
+desk image my photo and 
+http://jpg2png.com/ and https://tinypng.com/
+
+sound enemy emerge.wav https://www.freesound.org/people/Cabeeno%20Rossley/sounds/126415/
+
+bottle break http://opengameart.org/content/breaking-bottle
+
+items http://opengameart.org/content/drawn-rpg-inventory-icons-for-ardentryst-by-jordan-trudgett cc 3.0 license
+
+music http://opengameart.org/content/caketown-cuteplayful or http://opengameart.org/content/happy-arcade-tune or http://opengameart.org/content/loop-house-in-a-forest
---
+++ darnCatScripts.js
@@ -0,0 +1,185 @@
+$(function() {
+
+  $cat = $(".cat");
+  $shelf = $('.shelf')
+  $item = $('.item');
+
+  shelfWidth = $shelf.width;
+  shelfHeight = $shelf.height;
+
+  updateCounter()
+  setItemLoops()
+  setCatClick()
+
+  var spawnInterval = 5000;
+
+  function spawnItem() {
+    var xPos = Math.random() * ($shelf.width - $item.width) + 1;
+    var yPos = Math.random() * ($shelf.height - $item.height) +1;
+    // this needs to spawn items.
+  }
+  setInterval(spawnItem, 4000);
+
+});
+
+var $cat;
+var $shelf;
+var $item;
+var shelfWidth;
+var shelfHeight;
+var counter = 0;
+
+const items = [
+ 'assets/ArdentrystInvItems/Ardentryst-annelplate.png',
+ 'assets/ArdentrystInvItems/Ardentryst-AnneludineLeggings.png',
+ 'assets/ArdentrystInvItems/Ardentryst-BlueHood.png',
+ 'assets/ArdentrystInvItems/Ardentryst-Broadsword.png',
+ 'assets/ArdentrystInvItems/Ardentryst-BronzeBattleAxe.png',
+ 'assets/ArdentrystInvItems/Ardentryst-BronzeBattleAxe2.png',
+ 'assets/ArdentrystInvItems/Ardentryst-BronzeBattleAxe3.png',
+ 'assets/ArdentrystInvItems/Ardentryst-BronzeGladius.png',
+ 'assets/ArdentrystInvItems/Ardentryst-Chemical.png',
+ 'assets/ArdentrystInvItems/Ardentryst-Chemical2.png',
+ 'assets/ArdentrystInvItems/Ardentryst-Crossbow.png',
+ 'assets/ArdentrystInvItems/Ardentryst-Crossbow2.png'
+];
+
+var setCatClick = function() {
+  $cat.click(function () {
+    // console.log($item);
+    const catOrigin = $cat.position().left;
+    var itemOrigin = $item.position().left;
+    var diff = (itemOrigin - catOrigin) + $cat.width();
+    $cat.animate( { left: diff }, 'slow', function() {
+      // compare cat and item positions.
+      if ($cat.position().left >= $item.position().left) {
+        $item.animate( {left: (Math.random() * 200) + $item.position().left} );
+        $cat.animate( {left: catOrigin}, 'slow');
+      };
+      // if it's off the page, it falls
+      if ((($item.position().left) + 111) >= $shelf.width()) {
+        $item.delay(200).animate( {top: '80%'}, {duration: 2000} ) &&
+        $item.fadeOut('slow') &&
+        setTimeout(function() { $item.remove(); }, 3000);
+        updateCounter();
+      };
+      // check for loss
+      // checkGameStatus()
+    });
+  });
+}
+
+var updateCounter = function() {
+  counter++
+  $('.counter').html( counter + ' items have been knocked off.');
+}
+
+var checkGameStatus = function() {
+  var playerLost = checkForLoss()
+  var playerWon = checkForWin()
+
+  if (playerLost) {
+    playerLoses();
+  } else if (playerWon) {
+    playerWins();
+  }
+};
+
+// if you pass this a set of items on the page you made with .item,
+// it will attach a click to each one.
+var setItemLoops = function() {
+  itemElements = $(".item")
+
+  for (var item of itemElements) {
+    setElementClick($(item))
+
+    $(item).on("remove", function () {
+      alert("Element was removed");
+    });
+  }
+}
+
+var setElementClick = function (element) {
+  element.click(function() {
+    element.animate( {left:'-=100'} );
+    // checkGameStatus()
+  });
+}
+
+var checkForLoss = function() {
+  // check win condition
+  counter > 9;
+};
+
+var playerLoses = function() {
+  alert("ya lost, dingus");
+};
+
+var playerWins = function() {
+  alert("ya won, great job");
+};
+
+var checkForWin = function() {
+  // all items are off the screen and the cat can't cause more chaos
+};
+
+// const items = [
+//  'assets/ArdentrystInvItems/Ardentryst-annelplate.png',
+//  'assets/ArdentrystInvItems/Ardentryst-AnneludineLeggings.png',
+//  'assets/ArdentrystInvItems/Ardentryst-BlueHood.png',
+//  'assets/ArdentrystInvItems/Ardentryst-Broadsword.png',
+//  'assets/ArdentrystInvItems/Ardentryst-BronzeBattleAxe.png',
+//  'assets/ArdentrystInvItems/Ardentryst-BronzeBattleAxe2.png',
+//  'assets/ArdentrystInvItems/Ardentryst-BronzeBattleAxe3.png',
+//  'assets/ArdentrystInvItems/Ardentryst-BronzeGladius.png',
+//  'assets/ArdentrystInvItems/Ardentryst-Chemical.png',
+//  'assets/ArdentrystInvItems/Ardentryst-Chemical2.png',
+//  'assets/ArdentrystInvItems/Ardentryst-Crossbow.png',
+//  'assets/ArdentrystInvItems/Ardentryst-Crossbow2.png',
+//  'assets/ArdentrystInvItems/Ardentryst-Crossbow3.png',
+//  'assets/ArdentrystInvItems/Ardentryst-EnchantedSickle.png',
+//  'assets/ArdentrystInvItems/Ardentryst-EnchantedSickleb.png',
+//  'assets/ArdentrystInvItems/Ardentryst-EnchantedSicklec.png',
+//  'assets/ArdentrystInvItems/Ardentryst-frostrobe.png',
+//  'assets/ArdentrystInvItems/Ardentryst-frostskirt.png',
+//  'assets/ArdentrystInvItems/Ardentryst-GreenHood.png',
+//  'assets/ArdentrystInvItems/Ardentryst-gripboots.png',
+//  'assets/ArdentrystInvItems/Ardentryst-NeptheneFibreLeggings.png',
+//  'assets/ArdentrystInvItems/Ardentryst-nepthenevest.png',
+//  'assets/ArdentrystInvItems/Ardentryst-pendant.png',
+//  'assets/ArdentrystInvItems/Ardentryst-pendant2.png',
+//  'assets/ArdentrystInvItems/Ardentryst-pendant3.png',
+//  'assets/ArdentrystInvItems/Ardentryst-pendant4.png',
+//  'assets/ArdentrystInvItems/Ardentryst-pendant5.png',
+//  'assets/ArdentrystInvItems/Ardentryst-Potion.png',
+//  'assets/ArdentrystInvItems/Ardentryst-Potion2.png',
+//  'assets/ArdentrystInvItems/Ardentryst-Potion3.png',
+//  'assets/ArdentrystInvItems/Ardentryst-Potion4.png',
+//  'assets/ArdentrystInvItems/Ardentryst-PotionB.png',
+//  'assets/ArdentrystInvItems/Ardentryst-PotionB2.png',
+//  'assets/ArdentrystInvItems/Ardentryst-PotionB3.png',
+//  'assets/ArdentrystInvItems/Ardentryst-PotionB4.png',
+//  'assets/ArdentrystInvItems/Ardentryst-ring1.png',
+//  'assets/ArdentrystInvItems/Ardentryst-ring2.png',
+//  'assets/ArdentrystInvItems/Ardentryst-ring3.png',
+//  'assets/ArdentrystInvItems/Ardentryst-ring4.png',
+//  'assets/ArdentrystInvItems/Ardentryst-ring5.png',
+//  'assets/ArdentrystInvItems/Ardentryst-ring6.png',
+//  'assets/ArdentrystInvItems/Ardentryst-Shell.png',
+//  'assets/ArdentrystInvItems/Ardentryst-silverpouch.png',
+//  'assets/ArdentrystInvItems/Ardentryst-Skull.png',
+//  'assets/ArdentrystInvItems/Ardentryst-sorchat.png',
+//  'assets/ArdentrystInvItems/Ardentryst-sorcrobe.png',
+//  'assets/ArdentrystInvItems/Ardentryst-sorcskirt.png',
+//  'assets/ArdentrystInvItems/Ardentryst-StaffA.png',
+//  'assets/ArdentrystInvItems/Ardentryst-StaffB.png',
+//  'assets/ArdentrystInvItems/Ardentryst-StaffC.png',
+//  'assets/ArdentrystInvItems/Ardentryst-SteelCap.png',
+//  'assets/ArdentrystInvItems/Ardentryst-SteelGladius.png',
+//  'assets/ArdentrystInvItems/Ardentryst-SteelHalberd.png',
+//  'assets/ArdentrystInvItems/Ardentryst-SteelHalberd2.png',
+//  'assets/ArdentrystInvItems/Ardentryst-SteelHalberd3.png',
+//  'assets/ArdentrystInvItems/Ardentryst-SteelRoundHelm.png',
+//  'assets/ArdentrystInvItems/Ardentryst-Tongue.png',
+//  'assets/ArdentrystInvItems/Ardentryst-wingboots.png'
+// ];
